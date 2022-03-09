28e3c8bff4a9eac5b53742564e46cbef4f22d5f9
prasadgoregaonkar <46897641+prasadgoregaonkar@users.noreply.github.com>
2022-03-09 21:47:40 +0530

    Add files via upload

---
+++ Hartals.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+long long ar[3659],n,p,i,n,sum,t,j;
+cin>>t;
+while(t--)
+{
+cin>>nn>>p;
+for(i=1i<=n;i++)
+ar[i]=0;
+for(i=1;i<=p;i++){
+cin>>x;
+
+for(j=x;j<=n;j+=x){
+ar[j]=1;
+}
+}
+sum=0;
+for(i=1;i<=n;i++){
+if(i%7!=0 && i%7=6){
+sum+arr[i];
+}
+cout<<sum<<end1;
+}
+return 0;
+}
+}
